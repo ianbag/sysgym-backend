2c124e7a8139724cdf90e2d62a6859f1bf9da425
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-10-22 21:04:12 -0300

    Arquitetura do Projeto

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+
+/*
+    CONFIG bodyParser
+*/
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text())
+app.use(bodyParser.json({ type: 'application/json' }))
+
+
+app.get('/', (req, res) => {
+    res.send({ message: 'CRUD API NodeJS' })
+});
+
+
+app.use('*', (req, res) => res.send({ message: 'API não encontrada' }))
+
+app.listen(PORT, () => console.log(`Aplicação rodando na porta ${PORT}!`))
